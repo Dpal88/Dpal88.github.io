8e4b367fc74ab84c99a6dee008716b3112919e0e
Dominic Palomino <MynameisDomPal@gmail.com>
2024-10-08 15:02:37 -0500

    fixed ham menu & added event to nav anchor elements
    fixed issue with ham menu where when screen size would change from above 600px to under 600px, nav links would appear even if hamIcon was never clicked.
    
    Also added an event listener to all nav anchor elements whenever they are clicked, which will then close the nav menu.

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const hamBtn = document.querySelector('#ham-btn');
 const hamIcon = document.querySelector("#ham-icon");
 const closeIcon = document.querySelector('#close-icon');
 const navLinks = document.querySelector('#nav-links');
+const anchors = document.querySelectorAll('.nav-links span a');
 
 // form input variables
 const inputMsg = document.querySelector('textarea');
@@ -148,86 +149,41 @@ inputMsg.addEventListener('keydown', e => {
 
 // hamburger menu functions ----------------------------------------------------------------------------------
 
-function hamburgerMenu () {
+function hamMenuActive () {
+    if ( hamIcon.style.display === 'none' || hamIcon.classList.contains('hide') ) {
+        hamIcon.classList.remove('hide');
+        closeIcon.classList.remove('active-block');
+        navLinks.classList.remove('active-flex');
+        console.log("remove close icon");
 
-    // if hamIcon display is not set to none,
-    // then hide hamIcon, and show closeIcon & navLinks.
-    if (hamIcon.style.display != 'none') {
-
-        hamIcon.style.display = "none";
-        closeIcon.style.display = "block";
-        navLinks.style.display = "flex";
-        // navLinks.classList.add("active-flex");
-
-      // else show hamIcon & hide closeIcon & navLinks.  
+        
     } else {
-
-        hamIcon.style.display = "block";
-        closeIcon.style.display = "none";
-        navLinks.style.display = "none";
-        // navLinks.classList.add("hide");
-
+        hamIcon.classList.add('hide');
+        closeIcon.classList.add('active-block');
+        navLinks.classList.add('active-flex');
+        console.log("remove ham icon");
     }
 }
 
-function changeScreen() {
-    if (window.screen.width >= 600) {
-        navLinks.classList.remove("hide");
-        navLinks.classList.add("active-flex");
-        console.log('over 600px');
-    } else if (window.screen.width <= 599 && hamIcon.style.display != 'none') {
-        navLinks.classList.remove("active-flex");
-        navLinks.classList.add("hide");
-        console.log('under 600px');
-
-    }
-}
-
-// function hamMenu() {
-//     if (hamIcon.style.display != 'none') {
-//         hamIcon.classList.add("hide");
-//         closeIcon.classList.add("active-block");
-//         navLinks.classList.add("active-flex");
-//     } else {
-//         hamIcon.classList.remove("hide");
-//         closeIcon.classList.remove("active-block");
-//         navLinks.classList.remove("active-flex");
-//     }
-// }
-
-function screenResize() {
-    // if window screen is greater than or equal to 600,
-    // then set navLinks to display flex. else if hamIcon
-    // is displayed, set navLinks to display none.
-    // & if closeIcon is showing, set navLinks to display flex.
-    // (this fixes any issues if user goes back and fourth between screen sizes)
-    if (window.screen.width <= 599 && hamIcon.style.display != 'none') {
-        navLinks.style.display = "none";
-        console.log('over 600px');
-    } else if (window.screen.width >= 600) {
-        navLinks.style.display = "flex";
-        console.log('over 600px');
-    }
-
-    
-    // else if (hamIcon.style.display != 'none') {
-    //     navLinks.style.display = "none";
-    // } else if (closeIcon.style.display != 'none') {
-    //     navLinks.style.display = "flex";
-    // }
-}
-
-
+anchors.forEach(anchor => {
+    anchor.addEventListener('click', e => {
+        hamIcon.classList.remove('hide');
+        closeIcon.classList.remove('active-block');
+        navLinks.classList.remove('active-flex');
+    })
+})
 
 hamBtn.addEventListener('click', e => {
 
-    hamburgerMenu();
+    // hamburgerMenu();
+    hamMenuActive();
 })
 
 
-addEventListener("resize", (e) => {
-    screenResize();
-    // changeScreen();
-})
+// addEventListener("resize", (e) => {
+//     changeScreenClass();
+//     screenResize();
+//     changeScreen();
+// })
 
 // try enabling navLinks with classes instead of setting style inline
